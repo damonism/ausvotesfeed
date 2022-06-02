0b670e3834057f8f3ab2a1711fd134b8a60c6733
Damon Muller <admin@ausvotes.org>
2022-06-02 13:21:23 +1000

    Added total votes to get by div

--- R/get_mediafeed_votes_div.R
+++ R/get_mediafeed_votes_div.R
@@ -1,7 +1,8 @@
-#' Get votes by polling place
+#' Get votes by division
 #'
 #' Generic function for downloading various vote types (first preference,
-#' two-candidate preferred or two-party preferred) by polling place.
+#' two-candidate preferred, two-party preferred or total votes) by polling
+#' place.
 #'
 #' Note that this iteration of this function does not have any real error
 #' checking, and may stop working if it receives unexpected input. The data
@@ -10,7 +11,8 @@
 #'
 #' @param xml A pointer to an XML media feed object
 #' @param type One of either \code{fp} (first preference), \code{tcp}
-#'   (two-candidate preferred) or \code{tpp} (two party preferred).
+#'   (two-candidate preferred), \code{tpp} (two party preferred) or \code{total}
+#'   (total first preference votes).
 #'
 #' @return A \code{data.frame} with a \code{DivisionId} column and at least
 #'   current, historic, percentage, swing and matched historic votes.
@@ -32,7 +34,7 @@ get_mediafeed_votes_div <- function(xml, type) {
   tmp_div_nodes <- xml_find_all(xml, "d1:Results/d1:Election/d1:House/d1:Contests/d1:Contest")
 
   type <- tolower(type)
-  if(type == "fp") {
+  if (type == "fp") {
     tmp_cand_nodes <- xml_find_all(tmp_div_nodes, "d1:PollingDistrictIdentifier|d1:FirstPreferences/d1:Candidate|d1:FirstPreferences/d1:Ghost")
     tmp_vote_prefix <- "FP"
     tmp_list_ints_type <- c("CandidateId", "FP.Historic", "FP.MatchedHistoric", "FP.Votes")
@@ -42,13 +44,18 @@ get_mediafeed_votes_div <- function(xml, type) {
     tmp_vote_prefix <- "TCP"
     tmp_list_ints_type <- c("CandidateId", "TCP.Historic", "TCP.MatchedHistoric", "TCP.Votes", "TCP.MatchedHistoricFirstPrefsIn")
     tmp_list_num_type <- c("TCP.Percentage", "TCP.Swing")
-  } else if(type == "tpp") {
+  } else if (type == "tpp") {
     tmp_cand_nodes <- xml_find_all(tmp_div_nodes, "d1:PollingDistrictIdentifier|d1:TwoPartyPreferred/d1:Coalition")
     tmp_vote_prefix = "TPP"
     tmp_list_ints_type <- c("CoalitionId", "TPP.Historic", "TPP.MatchedHistoric", "TPP.Votes")
     tmp_list_num_type <- c("TPP.Percentage", "TPP.Swing")
+  } else if (type == "total") {
+    tmp_cand_nodes <- xml_find_all(tmp_div_nodes, "d1:PollingDistrictIdentifier|d1:FirstPreferences/d1:Formal|d1:FirstPreferences/d1:Informal|d1:FirstPreferences/d1:Total")
+    tmp_vote_prefix <- "FP"
+    tmp_list_ints_type <- c("CandidateId", "FP.Historic", "FP.MatchedHistoric", "FP.Votes")
+    tmp_list_num_type <- c("FP.Percentage", "FP.Swing")
   } else {
-    stop("type must be one of 'fp', 'tcp' or 'tpp'.")
+    stop("type must be one of 'fp', 'tcp', 'tpp' or 'total'.")
   }
 
   tmp_df <- data.frame(DivisionId = xml_attr(tmp_cand_nodes, "Id"),
@@ -61,6 +68,8 @@ get_mediafeed_votes_div <- function(xml, type) {
                          CoalitionId = xml_attr(xml_find_all(tmp_cand_nodes, "d1:CoalitionIdentifier"), "Id"),
                          CoalitionCode = xml_attr(xml_find_all(tmp_cand_nodes, "d1:CoalitionIdentifier"), "ShortCode"),
                          stringsAsFactors = FALSE)
+  } else if (type == "total") {
+    tmp_df$CandidateId <- NA
   } else {
     tmp_df <- data.frame(tmp_df,
                          CandidateId = xml_attr(xml_find_all(tmp_cand_nodes, "eml:CandidateIdentifier"), "Id"),
